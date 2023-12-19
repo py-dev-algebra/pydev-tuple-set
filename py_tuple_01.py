2e6719f9f2c12480a8bcc0e49b2d3a697288d3fa
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2023-12-19 18:14:47 +0100

    Summary

---
+++ py_tuple_01.py
@@ -0,0 +1,17 @@
+lista = ['Pero Peric', 'Ana Anic', 'Iva Ivic']
+rjecnik = {
+    'first_name': 'Pero',
+    'last_name': 'Peric',
+    'year_of_birth': 1998
+}
+
+#              0               1          2
+n_terac = ('Pero Peric', 'Ana Anic', 'Iva Ivic')
+
+for element in n_terac:
+    print(element, end='; ')
+print()
+
+print(n_terac[1])
+
+n_terac
